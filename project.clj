85881e2145b0c7e83c6a7d78de6fa6094701bc9f
Peter Westmacott <peter.westmacott@plumbee.co.uk>
2016-04-01 14:46:26 +0100

    Adding licence and project file.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clojure-hamcrest "0.1"
+  :description "A Clojure version of the Hamcrest matcher library."
+  :url "http://plumbee.com"
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
